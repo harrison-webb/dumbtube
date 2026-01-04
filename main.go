829a347b46715f799cfa5eb6cb8cee9cfb36e258
Harrison Webb <harrison.dev.00@gmail.com>
2026-01-04 15:58:27 -0700

    unmarshal rss feed into struct

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
@@ -14,7 +16,7 @@ func main() {
 	// - download RSS feed
 	// - parse RSS feed (don't want shorts included)
 
-	testURLString := "https://www.youtube.com/@vanneistat"
+	testURLString := "https://www.youtube.com/@tanksfornothin/"
 	testURL, err := url.Parse(testURLString)
 	if err != nil {
 		panic(err)
@@ -22,9 +24,15 @@ func main() {
 
 	channelRSSURL := getRSSURLFromChannelURL(testURL)
 	feed, _ := fetchRSSFeed(channelRSSURL)
-	fmt.Print(string(feed))
+	parsedFeed := parseRSSFeed(feed)
+	out, err := json.MarshalIndent(parsedFeed, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(out))
 }
 
+// getRSSURLFromChannelURL returns the URL of the RSS feed of a youtube channel when given that channel's homepage URL
 func getRSSURLFromChannelURL(channelURL *url.URL) *url.URL {
 	// add User-Agent header to request because claude said youtube might be rate limiting me without one
 	client := &http.Client{}
@@ -50,7 +58,8 @@ func getRSSURLFromChannelURL(channelURL *url.URL) *url.URL {
 		panic(err)
 	}
 
-	r := regexp.MustCompile("channel_id=[a-zA-Z0-9]{24}")
+	// regex pattern: channel_id{24 characters}
+	r := regexp.MustCompile(`channel_id=\S{24}`)
 	match := r.Find(body)
 	channelId := string(match[len(match)-24:])
 
@@ -62,6 +71,7 @@ func getRSSURLFromChannelURL(channelURL *url.URL) *url.URL {
 	return RSSURL
 }
 
+// fetchRSSFeed returns the RSS feed contents of a given youtube channel RSS URL
 func fetchRSSFeed(rssURL *url.URL) ([]byte, error) {
 	// add User-Agent header to request because claude said youtube might be rate limiting me without one
 	client := &http.Client{}
@@ -77,4 +87,53 @@ func fetchRSSFeed(rssURL *url.URL) ([]byte, error) {
 	defer response.Body.Close()
 
 	return io.ReadAll(response.Body)
+}
+
+func parseRSSFeed(rssBody []byte) feed {
+	var resultFeed feed
+	xml.Unmarshal(rssBody, &resultFeed)
+	return resultFeed
+}
+
+// feed is an array of entries
+// each entry has:
+// 		- id -> string
+//		- title -> string
+//		- link -> URL
+// 		- author -> string
+//		- date -> date
+// 		- media:thumbnail -> URL
+
+type feed struct {
+	// XMLName xml.Name `xml:"feed"`
+	Entries []entry `xml:"entry"`
+}
+
+type entry struct {
+	// XMLName     xml.Name   `xml:"entry"`
+	ID          string     `xml:"id"`
+	Title       string     `xml:"title"`
+	Link        link       `xml:"link"` // TODO: format looks like: <link rel="alternate" href="https://www.youtube.com/watch?v=dUDpjxPm8os"/> and I just want to pull out the href attribute string
+	Author      author     `xml:"author"`
+	PublishDate string     `xml:"published"`
+	MediaGroup  mediaGroup `xml:"http://search.yahoo.com/mrss/ group"`
+}
+
+type link struct {
+	Rel  string `xml:"rel,attr"`
+	Href string `xml:"href,attr"`
+}
+
+type mediaGroup struct {
+	Thumbnail mediaThumbnail `xml:"http://search.yahoo.com/mrss/ thumbnail"`
+}
+
+type mediaThumbnail struct {
+	URL string `xml:"url,attr"`
+}
+
+type author struct {
+	// XMLName    xml.Name `xml:"author"`
+	Name       string `xml:"name"`
+	ChannelURI string `xml:"uri"`
 }
