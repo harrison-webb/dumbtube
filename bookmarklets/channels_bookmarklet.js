1beae13bec21ad05cec757b27f8ab715e1949f87
Harrison Webb <harrison.dev.00@gmail.com>
2026-01-06 21:51:07 -0700

    channels bookmarklet working

--- bookmarklets/channels_bookmarklet.js
+++ bookmarklets/channels_bookmarklet.js
@@ -3,11 +3,25 @@
 /* All comments must be block comments in order for build_bookmarklets.sh to properly convert code to one line*/
 (function() {
 
+    const STORAGE_KEY = "dumbtube_ingest_endpoint";
     const blacklist = ["/@nytimes", "/@fox43"];
+
+    function getEndpoint(){
+        let endpoint = localStorage.getItem(STORAGE_KEY);
+        if (!endpoint) {
+            endpoint = prompt("Enter Dumbtube channel list ingest endpoint url: ");
+            if (!endpoint) return null;
+            localStorage.setItem(STORAGE_KEY, endpoint);
+        }
+        return endpoint;
+    }
 
     function isVisibleAndInSubscriptions(element) {
         return !element.hasAttribute("hidden") && !blacklist.includes(element.getAttribute("href"));
     }
+
+    const endpoint = getEndpoint();
+    if (!endpoint) return;
 
     alert("Be sure to click 'Show more' on your subscriptions sidebar so that all subscribed channels are shown");
 
@@ -20,5 +34,11 @@
     
     console.log(channels);
 
-    /* TODO: send json to endpoint */
+    /* send json to endpoint */
+    fetch(endpoint, {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ channels })
+    });
 })();
